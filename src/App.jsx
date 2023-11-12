0ab04188213ad322b780818cf573e81dd7e066fa
Joan Scabino <joanscabinovosa@gmail.com>
2023-11-12 19:15:31 -0300

    Agrego de Item list y los detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
+import { Error404 } from "./components/Error404";
 
 import "./App.css"
 
+
 function App() {
   return (
-    <>  
-  <NavBar />
-  <ItemListContainer greeting="BIENVENIDO A FIXAMED"/>
-  </>
-);
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route 
+         path="/" element={<ItemListContainer greeting="BIENVENIDO A FIXAMED" />} />
+        <Route path="/category/ :id" element={<ItemListContainer greeting="BIENVENIDO A FIXAMED" />}
+        />
+
+        <Route path="/items/:id" element={<div>Linea Artroscopica</div>}/>
+        <Route path="*" element={<Error404/>}/>
+
+      </Routes>
+      {/*/<ItemListContainer greeting="BIENVENIDO A FIXAMED"/>*/}
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/components/Error404.jsx
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+
+export const Error404 =() => {
+
+    return(
+    <Container className='mt-5'>
+        <h1>Recurso No Encontrado</h1>
+    </Container>
+    );
+};
